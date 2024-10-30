a54c139e9760561b5bae42e1f49c697cbad2c59b
Shamima Sultana <ssultana211025@bscse.uiu.ac.bd>
2024-10-30 20:08:10 +0600

    property_management_DB II

---
+++ property_management_II.sql
@@ -0,0 +1,397 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2024 at 03:06 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `property_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `Admin_ID` int(11) NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `Feedback_ID` int(11) NOT NULL,
+  `Feedback_Date` date DEFAULT NULL,
+  `Ratings` int(11) DEFAULT NULL CHECK (`Ratings` between 1 and 5),
+  `Comment` text DEFAULT NULL,
+  `Tenant_ID` int(11) DEFAULT NULL,
+  `Landlord_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `landlord`
+--
+
+CREATE TABLE `landlord` (
+  `Landlord_ID` int(11) NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Phone` varchar(15) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `picture` mediumblob NOT NULL,
+  `account_type` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `Payment_ID` int(11) NOT NULL,
+  `Amount` decimal(10,2) DEFAULT NULL,
+  `Service_charge` decimal(10,0) NOT NULL,
+  `Discount` decimal(10,2) DEFAULT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  `Description` text DEFAULT NULL,
+  `Payment_Method` enum('Cash','Credit Card','Bank Transfer','Bkash') DEFAULT NULL,
+  `Tenant_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+CREATE TABLE `property` (
+  `property_ID` int(11) NOT NULL,
+  `st_no` int(11) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `Type` varchar(50) DEFAULT NULL,
+  `Size` decimal(10,2) DEFAULT NULL,
+  `Amenities` text DEFAULT NULL,
+  `Num_of_rooms` int(11) DEFAULT NULL,
+  `Rent` decimal(15,2) DEFAULT NULL,
+  `img1` mediumblob NOT NULL,
+  `img2` mediumblob NOT NULL,
+  `img3` mediumblob NOT NULL,
+  `img4` mediumblob NOT NULL,
+  `img5` mediumblob NOT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  `Landlord_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `Service_ID` int(11) NOT NULL,
+  `Type` varchar(100) DEFAULT NULL,
+  `Property_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_provider`
+--
+
+CREATE TABLE `service_provider` (
+  `Service_Provider_ID` int(11) NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Phone` varchar(15) DEFAULT NULL,
+  `Location` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_request`
+--
+
+CREATE TABLE `service_request` (
+  `Request_ID` int(11) NOT NULL,
+  `Service_ID` int(11) DEFAULT NULL,
+  `Property_ID` int(11) DEFAULT NULL,
+  `Service_Date` date DEFAULT NULL,
+  `Details` text DEFAULT NULL,
+  `Labour_Cost` decimal(10,2) DEFAULT NULL,
+  `Tenant_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant`
+--
+
+CREATE TABLE `tenant` (
+  `Tenant_ID` int(11) NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Phone` varchar(15) DEFAULT NULL,
+  `Property_ID` int(11) DEFAULT NULL,
+  `Lease_Start_Date` date DEFAULT NULL,
+  `Lease_End_Date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitor`
+--
+
+CREATE TABLE `visitor` (
+  `visitor_ID` int(11) NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Phone` varchar(15) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visit_request`
+--
+
+CREATE TABLE `visit_request` (
+  `Visit_Request_ID` int(11) NOT NULL,
+  `App_ID` int(11) DEFAULT NULL,
+  `Visitor_ID` int(11) DEFAULT NULL,
+  `Property_ID` int(11) DEFAULT NULL,
+  `App_Date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`Admin_ID`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`Feedback_ID`),
+  ADD KEY `Tenant_ID` (`Tenant_ID`),
+  ADD KEY `Landlord_ID` (`Landlord_ID`);
+
+--
+-- Indexes for table `landlord`
+--
+ALTER TABLE `landlord`
+  ADD PRIMARY KEY (`Landlord_ID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`Payment_ID`),
+  ADD KEY `Tenant_ID` (`Tenant_ID`);
+
+--
+-- Indexes for table `property`
+--
+ALTER TABLE `property`
+  ADD PRIMARY KEY (`property_ID`),
+  ADD KEY `Landlord_ID` (`Landlord_ID`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`Service_ID`),
+  ADD KEY `Property_ID` (`Property_ID`);
+
+--
+-- Indexes for table `service_provider`
+--
+ALTER TABLE `service_provider`
+  ADD PRIMARY KEY (`Service_Provider_ID`);
+
+--
+-- Indexes for table `service_request`
+--
+ALTER TABLE `service_request`
+  ADD PRIMARY KEY (`Request_ID`),
+  ADD KEY `Service_ID` (`Service_ID`),
+  ADD KEY `Property_ID` (`Property_ID`),
+  ADD KEY `Tenant_ID` (`Tenant_ID`);
+
+--
+-- Indexes for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD PRIMARY KEY (`Tenant_ID`),
+  ADD KEY `Property_ID` (`Property_ID`);
+
+--
+-- Indexes for table `visitor`
+--
+ALTER TABLE `visitor`
+  ADD PRIMARY KEY (`visitor_ID`);
+
+--
+-- Indexes for table `visit_request`
+--
+ALTER TABLE `visit_request`
+  ADD PRIMARY KEY (`Visit_Request_ID`),
+  ADD KEY `Visitor_ID` (`Visitor_ID`),
+  ADD KEY `Property_ID` (`Property_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `Admin_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `Feedback_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `landlord`
+--
+ALTER TABLE `landlord`
+  MODIFY `Landlord_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `Payment_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `property`
+--
+ALTER TABLE `property`
+  MODIFY `property_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `Service_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service_provider`
+--
+ALTER TABLE `service_provider`
+  MODIFY `Service_Provider_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service_request`
+--
+ALTER TABLE `service_request`
+  MODIFY `Request_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tenant`
+--
+ALTER TABLE `tenant`
+  MODIFY `Tenant_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visitor`
+--
+ALTER TABLE `visitor`
+  MODIFY `visitor_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visit_request`
+--
+ALTER TABLE `visit_request`
+  MODIFY `Visit_Request_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`Tenant_ID`) REFERENCES `tenant` (`Tenant_ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`Landlord_ID`) REFERENCES `landlord` (`Landlord_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`Tenant_ID`) REFERENCES `tenant` (`Tenant_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `property`
+--
+ALTER TABLE `property`
+  ADD CONSTRAINT `property_ibfk_1` FOREIGN KEY (`Landlord_ID`) REFERENCES `landlord` (`Landlord_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `service`
+--
+ALTER TABLE `service`
+  ADD CONSTRAINT `service_ibfk_1` FOREIGN KEY (`Property_ID`) REFERENCES `property` (`property_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `service_request`
+--
+ALTER TABLE `service_request`
+  ADD CONSTRAINT `service_request_ibfk_1` FOREIGN KEY (`Service_ID`) REFERENCES `service` (`Service_ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `service_request_ibfk_2` FOREIGN KEY (`Property_ID`) REFERENCES `property` (`property_ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `service_request_ibfk_3` FOREIGN KEY (`Tenant_ID`) REFERENCES `tenant` (`Tenant_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD CONSTRAINT `tenant_ibfk_1` FOREIGN KEY (`Property_ID`) REFERENCES `property` (`property_ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `visit_request`
+--
+ALTER TABLE `visit_request`
+  ADD CONSTRAINT `visit_request_ibfk_1` FOREIGN KEY (`Visitor_ID`) REFERENCES `visitor` (`visitor_ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `visit_request_ibfk_2` FOREIGN KEY (`Property_ID`) REFERENCES `property` (`property_ID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
